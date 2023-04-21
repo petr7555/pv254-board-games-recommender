495c61439db54aec110214aac2a3d243d3d010fe
Petr Janik <peta.janik@email.cz>
2023-04-21 13:33:34 +0200

    Add CORS

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.routers import games_router, recommendations_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(games_router.router)
 app.include_router(recommendations_router.router)
 
