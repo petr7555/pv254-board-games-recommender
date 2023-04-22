0852465a27fbab8facfd4fd0cc737c99224eae28
Petr Janik <peta.janik@email.cz>
2023-04-22 12:19:10 +0200

    Improve mock data

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,6 +1,8 @@
 import { rest } from 'msw';
 import Game from '../types/Game';
-import bestGamesByRank from './data/bestGamesByRank.json';
+import gamesOrderedByNameMock from './data/gamesOrderedByNameMock.json';
+import gamesOrderedByRankMock from './data/gamesOrderedByRankMock.json';
+import gamesOrderedByNumberOfRatingsMock from './data/gamesOrderedByNumberOfRatingsMock.json';
 import GamesSearchRequest from '../types/GamesSearchRequest';
 import PagedRequest from '../types/PagedRequest';
 import GamesResponse from '../types/GamesResponse';
@@ -11,39 +13,40 @@ const getPagedGames = async (allGames: Game[], offset: number, limit: number): P
   return { games, totalNumberOfGames };
 };
 
-// TODO
 const handlers = [
+  rest.post('*/recommendations/personalized', async (req, res, ctx) => {
+    const { offset, limit }: PagedRequest = await req.json();
+    // TODO
+    const response = await getPagedGames(gamesOrderedByRankMock, offset, limit);
+
+    return res(ctx.status(200), ctx.json(response));
+  }),
+
   rest.post('*/recommendations/top-rated', async (req, res, ctx) => {
     const { offset, limit }: PagedRequest = await req.json();
-    const response = await getPagedGames(bestGamesByRank, offset, limit);
+    const response = await getPagedGames(gamesOrderedByRankMock, offset, limit);
 
     return res(ctx.delay(300), ctx.status(200), ctx.json(response));
   }),
 
   rest.post('*/recommendations/most-rated', async (req, res, ctx) => {
     const { offset, limit }: PagedRequest = await req.json();
-    const response = await getPagedGames(bestGamesByRank, offset, limit);
-
-    return res(ctx.status(400), ctx.json(response));
-  }),
-
-  rest.post('*/recommendations/random', async (req, res, ctx) => {
-    const { offset, limit }: PagedRequest = await req.json();
-    const response = await getPagedGames(bestGamesByRank, offset, limit);
+    const response = await getPagedGames(gamesOrderedByNumberOfRatingsMock, offset, limit);
 
     return res(ctx.status(200), ctx.json(response));
   }),
 
-  rest.post('*/recommendations/personalized', async (req, res, ctx) => {
+  rest.post('*/recommendations/random', async (req, res, ctx) => {
     const { offset, limit }: PagedRequest = await req.json();
-    const response = await getPagedGames(bestGamesByRank, offset, limit);
+    // TODO
+    const response = await getPagedGames(gamesOrderedByRankMock, offset, limit);
 
     return res(ctx.status(200), ctx.json(response));
   }),
 
   rest.post('*/games', async (req, res, ctx) => {
     const { searchTerm, offset, limit }: GamesSearchRequest = await req.json();
-    const matchingGames = bestGamesByRank.filter(game => game.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    const matchingGames = gamesOrderedByNameMock.filter(game => game.name.toLowerCase().includes(searchTerm.toLowerCase()));
     const response = await getPagedGames(matchingGames, offset, limit);
 
     return res(ctx.status(200), ctx.json(response));
