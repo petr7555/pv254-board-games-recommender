f14f3530ca3c0d25140b92f0146a6f8e87ae01e7
Petr Janik <peta.janik@email.cz>
2023-04-22 18:31:14 +0200

    Make games search faster

--- server/app/routers/games_router.py
+++ server/app/routers/games_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
 from app.types.shared_types import PagedRequest, GamesResponse
-from app.utils.get_games_by_name import get_games_by_name
 from app.utils.get_paged_games import get_paged_games
+from app.utils.load_games_from_json import load_games_from_json
+from app.utils.relative_path_from_file import relative_path_from_file
 
 router = APIRouter(
     prefix="/games",
@@ -14,11 +15,15 @@ class GamesSearchRequest(PagedRequest):
     searchTerm: str
 
 
+games = load_games_from_json(relative_path_from_file(__file__, "../db/gamesOrderedByName.json"))
+
+
 @router.post("/")
 def get_games_by_name_route(request: GamesSearchRequest) -> GamesResponse:
     search_term = request.searchTerm
     offset = request.offset
     limit = request.limit
 
-    all_matching_games = get_games_by_name(search_term)
-    return get_paged_games(all_matching_games, offset, limit)
+    filtered_games = [game for game in games if search_term.lower() in game["name"].lower()]
+
+    return get_paged_games(filtered_games, offset, limit)
