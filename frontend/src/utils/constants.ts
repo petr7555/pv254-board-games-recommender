9fedfeff28e8a294b86e5df016bccfa7cee29a05
Petr Janik <peta.janik@email.cz>
2023-05-07 11:19:37 +0200

    Show both tfidf and latent factors carousels

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,11 +1,11 @@
 export const developmentServerUrl = 'http://127.0.0.1:8000';
 export const productionServerUrl = 'https://pv254-board-games-recommender-server.onrender.com';
 export const gamesEndpoint = '/games';
-export const personalizedRecommendationsEndpoint = '/recommendations/tfidf';
+export const tfidfRecommendationsEndpoint = '/recommendations/tfidf';
+export const latentFactorsRecommendationsEndpoint = '/recommendations/latent-factors';
 export const topRatedRecommendationsEndpoint = '/recommendations/top-rated';
 export const mostRatedRecommendationsEndpoint = '/recommendations/most-rated';
 export const randomRecommendationsEndpoint = '/recommendations/random';
-export const latentFactorsRecommendationsEndpoint = '/recommendations/latent-factors';
 
 export const appName = 'Board games recommender';
 export const maxRatingValue = 10;
